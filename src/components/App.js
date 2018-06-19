eb7b23c0abe0365be497721197ad50ee9444bf63
kat2018 <kat.vge@gmail.com>
2018-06-18 22:24:23 -0400

    updated files inside components folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Home from "./Home";
+import Search from './Search';
+import Results from "./Results";
+
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1>TV Maze</h1>
+        <Home />
+        <Search />
+        <hr/>
+        <Results />
       </div>
     );
   }
