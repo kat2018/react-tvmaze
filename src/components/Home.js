20ce03f78e9ca7358d1d255d55ec42ced0ec7053
kat2018 <kat.vge@gmail.com>
2018-06-18 23:27:33 -0400

    updated Home and Search

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import styled from "styled-components";
+
+const HomePage = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+
+  color: rgb(22, 117, 191);
+`;
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home Page</h1>
+                <HomePage>
                 <h1>TVMaze React</h1>
-                
+                </HomePage>
             </div>
         );
     }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 
 class Search extends Component {
     render() {
-        return (
-            <div>
-                <form action="get" method="get"></form>
-                <button></button>
-            </div>
-        );
+        return <div>
+            <form>
+              <label>
+                <input type="text" name="name" value="Enter a Movie Title" />
+              </label>
+                <input type="submit" value="Search" />
+            </form>
+          </div>;
     }
 }
 
